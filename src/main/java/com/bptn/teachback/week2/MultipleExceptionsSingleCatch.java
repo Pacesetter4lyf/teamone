5631556ca6407883db05753d89ab935fe9d1a756
pacesetter <pacesetter4lyf@gmail.com>
2024-06-20 21:30:38 -0400

    corrects error with the multipleexceptions single catch block

--- src/main/java/com/bptn/teachback/week2/MultipleExceptionsSingleCatch.java
+++ src/main/java/com/bptn/teachback/week2/MultipleExceptionsSingleCatch.java
@@ -3,8 +3,11 @@ package com.bptn.teachback.week2;
 public class MultipleExceptionsSingleCatch {
     public static void main(String[] args) {
 		try {
-	    	int array[] = new int[5];
-	    	array[10] = 30 / 0;
+			
+	    	int[] arr = {1, 2, 3, 3};
+	    	int fifthElement = arr[4];
+	    	arr[0] = 30 / 0;
+
 		} catch (ArithmeticException | ArrayIndexOutOfBoundsException e) {
 	    	System.out.println(e.getMessage());
 		}
